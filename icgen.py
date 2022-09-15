ec95ae662d588d5d0c27dfce1ffc008594e49f1d
Kemal Önen <kemal.oenen@student.uibk.ac.at>
2022-09-15 11:48:18 +0200

    linear valence angle update

--- icgen.py
+++ icgen.py
@@ -59,7 +59,7 @@ def initialize_angles(atoms):
     hits = []
     linear_hits = []
     for a,b,c in itertools.permutations(atoms, 3):
-        if angle.is_valid(a, b, c) and (not (c.symbol, b.symbol, a.symbol) in hits or not (c.symbol, b.symbol, a.symbol) in linear_hits):
+        if angle.is_valid(a, b, c) and not (c.symbol, b.symbol, a.symbol) in hits and not (c.symbol, b.symbol, a.symbol) in linear_hits:
             if np.isclose(angle.bond_angle(a.coordinates, b.coordinates, c.coordinates), np.pi):
                 linear_hits.append((a.symbol, b.symbol, c.symbol))
             else:
