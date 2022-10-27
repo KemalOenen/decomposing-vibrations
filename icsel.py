632612cda682145339d3da8c7a676cb8817e2464
Kemal Önen <kemal.oenen@student.uibk.ac.at>
2022-10-27 16:04:22 +0200

    dumb angle symmetry identification implemented

--- icsel.py
+++ icsel.py
@@ -1,4 +1,5 @@
 import itertools
+from os import remove
 import numpy as np
 import pprint
 import nomodeco
@@ -23,6 +24,35 @@ def avoid_double_oop(test_oop, used_out_of_plane) -> bool:
             return False
     return True
 
+def remove_enumeration(atom_list) -> list:
+    atom_list = [list(tup) for tup in atom_list]
+    for i in range(0, len(atom_list)):
+        for j in range(0, len(atom_list[i])):
+            atom_list[i][j] = ''.join(c for c in atom_list[i][j] if not c.isnumeric())
+    return atom_list
+
+
+def ic_rules_angles(used_angles, angles) -> bool:
+    used_angles = remove_enumeration(used_angles)
+    angles = remove_enumeration(angles)
+
+    angle_dict = {}
+    for ang in angles:
+        distinct_angle = tuple(ang)
+        if distinct_angle in angle_dict:
+            angle_dict[distinct_angle] += 1
+        else:
+            angle_dict[distinct_angle] = 1
+    
+    i = 0
+    for test_ang in angle_dict.keys():
+        if used_angles.count(list(test_ang)) == angle_dict.get(test_ang):
+            i += 1
+    
+    if i == len(angle_dict):
+        return True
+    else:
+        return False
 
 def test_completeness(CartesianF_Matrix, B, B_inv, InternalF_Matrix) -> bool:
     CartesianF_Matrix_check = np.transpose(B) @ InternalF_Matrix @ B
@@ -36,7 +66,7 @@ def matrix_norm(matrix, matrix_inv, p):
 
 def generate_all_possible_sets(n_atoms, idof, bonds, angles, linear_angles, out_of_plane, dihedrals):
     num_bonds = len(bonds)
-    
+
     ic_dict = dict()
 
     # TODO: make approach more elegant? -> include filter(), to have less but useful combinations
@@ -59,14 +89,15 @@ def generate_all_possible_sets(n_atoms, idof, bonds, angles, linear_angles, out_
                 if ic_subset[i] in dihedrals:
                     used_dihedrals.append(ic_subset[i])
             if (num_bonds + len(used_angles) + len(used_linear_angles) + len(used_out_of_plane) + len(used_dihedrals)) >= idof:
-                ic_dict[k] = {
-                    "bonds" : bonds,
-                    "angles" : used_angles,
-                    "linear valence angles" : used_linear_angles,
-                    "out of plane angles" : used_out_of_plane,
-                    "dihedrals" : used_dihedrals
-                }
-                k +=1
+                if ic_rules_angles(used_angles, angles):
+                    ic_dict[k] = {
+                        "bonds" : bonds,
+                        "angles" : used_angles,
+                        "linear valence angles" : used_linear_angles,
+                        "out of plane angles" : used_out_of_plane,
+                        "dihedrals" : used_dihedrals
+                    }
+                    k +=1
             used_angles, used_linear_angles, used_out_of_plane, used_dihedrals = [], [], [], []
     print(len(ic_dict), "internal coordinate sets have been generated.")
     return ic_dict
