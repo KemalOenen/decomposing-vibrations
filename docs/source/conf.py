f42390ce0b166bc368b9759d80956c96982198f8
LukasMeinschad <lukas.meinschad@gmail.com>
2024-10-08 22:51:29 +0200

    test api

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,14 +10,23 @@ import sys
 from pathlib import Path
 
 # Autodoc Inport
-current_dir = os.path.abspath(os.path.dirname(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, '../..'))
-module_path = os.path.join(parent_dir,"nomodeco","libraries")
-nomodeco_path = os.path.join(parent_dir,"nomdeco")
-sys.path.append(module_path)
-#sys.path.append(nomodeco_path)
-sys.path.append(str((Path(__file__).parents[2].absolute() / "nomodeco")))
+#current_dir = os.path.abspath(os.path.dirname(__file__))
+#parent_dir = os.path.abspath(os.path.join(current_dir, '../..'))
+#module_path = os.path.join(parent_dir,"nomodeco","libraries")
+#nomodeco_path = os.path.join(parent_dir,"nomdeco")
+#sys.path.append(module_path)
+##sys.path.append(nomodeco_path)
+#sys.path.append(str((Path(__file__).parents[2].absolute() / "nomodeco")))
 
+
+# Set the path relative to the location of conf.py
+current_dir = Path(__file__).parent.resolve()  # Directory where conf.py is located
+module_path = current_dir / '../../nomodeco/libraries'
+nomodeco_path = current_dir / '../../nomodeco'
+
+# Convert Path objects to strings and append them to sys.path
+sys.path.append(str(module_path.resolve()))
+sys.path.append(str(nomodeco_path.resolve()))
 
 project = 'nomodeco'
 copyright = '2024, Lukas'
